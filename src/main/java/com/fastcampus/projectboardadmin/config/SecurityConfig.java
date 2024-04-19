0ea9132d1b0e1c612b2c00eb699b4b23a2d50109
RobertRyu <ckue21@gmail.com>
2024-04-19 14:47:39 +0900

    #13 - 로그인 페이지 및 기초 OAuth 2.0 인증 옵션 추가
    헤더 네비게이션 바 레이아웃 템플릿에
    타임리프 태그 `sec:authorize`를 활용하여
    인증 상태에 따라 노출되는 로그인, 로그아웃 버튼 추가
    
    버튼 클릭하여 카카오 로그인 페이지를 호출 시도하면
    카카오 로그인 페이지로 이동해야 하므로
    스프링 시큐리티 oauth 의 기본 기능을 켜서
    해당 기능이 반응하도록 함
    뷰 구현으로 아직 얼개만 잡은 상태

--- src/main/java/com/fastcampus/projectboardadmin/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboardadmin/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
                 )
                 .formLogin(withDefaults())
                 .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(withDefaults())
                 .build();
     }
 
